edaf5196d41233f6df874408d605be320204d60e
Britta <bbuhnemann@gmail.com>
2012-02-20 16:02:31 -0800

    Initial Commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+    puts "hello"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrittaTest::Application.config.secret_token = 'ff319e2298a62bc3ee3064f9043cd609c6b30ded3da32bb448ba58964972ba7a020b176944d1e0573b0aa4d59a6488727e8af7d97d7032d41c89b04b1f88ed2a'
